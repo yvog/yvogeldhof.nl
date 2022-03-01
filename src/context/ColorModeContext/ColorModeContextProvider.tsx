77fdf7644c132a85e62ba18ba2f5c6e94c5e05a1
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2022-03-01 16:58:02 +0100

    feat: load color mode based on saved preference

--- src/context/ColorModeContext/ColorModeContextProvider.tsx
+++ src/context/ColorModeContext/ColorModeContextProvider.tsx
@@ -1,28 +1,66 @@
 import { PaletteMode, useMediaQuery } from '@mui/material'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { ColorModeContext } from './useColorModeContext'
 
 type ColorModeContextProviderProps = {
   children: React.ReactNode
 }
 
+function getColorModePreference(): PaletteMode | null {
+  try {
+    const preference = window.localStorage.getItem(
+      'yvogeldhof_nl_colormode_preference'
+    ) as PaletteMode
+
+    return preference
+  } catch (error) {
+    console.error('Could not get color mode preference: ', error)
+  }
+
+  return null
+}
+
+function setColorModePreference(mode: PaletteMode): boolean {
+  try {
+    window.localStorage.setItem('yvogeldhof_nl_colormode_preference', mode)
+    return true
+  } catch (error) {
+    console.error('Could not set color mode preference: ', error)
+  }
+
+  return false
+}
+
 export const ColorModeContextProvider = (props: ColorModeContextProviderProps) => {
   const { children } = props
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  const defaultColorMode: PaletteMode = 'dark' // todo: preference from localstorage ?? 'dark'
-  const [mode, setMode] = useState<PaletteMode>(
-    prefersDarkMode ? 'dark' : defaultColorMode
-  )
+  const [mode, setMode] = useState<PaletteMode | undefined>(undefined)
+
+  useEffect(() => {
+    if (mode != undefined) return
+
+    const colorModePreference = getColorModePreference()
+    const colorModeFallback = prefersDarkMode ? 'dark' : 'light'
+    const defaultColorMode: PaletteMode = colorModePreference ?? colorModeFallback
+
+    setMode(defaultColorMode)
+  }, [mode, prefersDarkMode])
 
   const colorMode = useMemo(
     () => ({
-      mode,
+      mode: mode ?? 'light',
       toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) => (prevMode === 'light' ? 'dark' : 'light'))
+        setMode((prevMode: PaletteMode | undefined) => {
+          const newMode = prevMode === 'light' ? 'dark' : 'light'
+          setColorModePreference(newMode)
+          return newMode
+        })
       },
     }),
     [mode]
   )
+
+  if (mode == undefined) return <></>
 
   return (
     <ColorModeContext.Provider value={colorMode}>{children}</ColorModeContext.Provider>
